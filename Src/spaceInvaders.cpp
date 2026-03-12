19d797a766cffd147b04c34b936a25296fc186e7
Saad <saadfenixel@gmail.com>
2026-03-12 14:30:30 +0500

    Update GameState & Add InputMode enum

--- Src/spaceInvaders.cpp
+++ Src/spaceInvaders.cpp
@@ -20,11 +20,27 @@ using std::string;
 */
 
 
-enum GameState{
-    MENU,
-    PLAYING,
-    PAUSED,
-    GAMEOVER
+enum GameState{                 // an ENUM to indicate the current state of the game
+    // mainMenu
+        MENU,                   // the mainMenu screen of the game; contains the following 👇
+            PLAY,               // contains options such as playerName, difficulty, spaceShip skin etc
+            SHOP,               // buy diff spaceShip skins, aliens' skins, SFX perhaps
+            HISTORY,            // show gameHistory (using sqlite)
+            LEADERBOARDS,       // display leaderBoards with top10 players (using sqlite)
+            SETTINGS,           // game settings such as fullscreen, SFX volume, playerInputMode, opacity etc (similar to Mr. Pong's)
+
+    // gamePlay
+        PLAYING,                // actual gamePlay state
+        PAUSED,                 // while game is paused inside PLAYING
+        GAMEOVER,               // when game ends inside PLAYING; shows outcome, score, aliens-defeated, coins earned etc
+
+    // windowShouldClose()
+        CLOSEWINDOW             // when user chooses to close the window; plays a meme or similar
+};
+enum InputMode{                 // an ENUM to indicate the chosen playerInputMode (altered in settings)
+    WASD,                       
+    ARROW,                      
+    MOUSE
 };
 
 class State{                                    // an abstract class to be inheriteed by all gameState subclasses
@@ -145,9 +161,18 @@ int main()
         UnloadImage(favicon);
     }
 
+    Game game;
+
     while (!WindowShouldClose()){
+        // updating
+        game.update();
+
+        // drawing
         BeginDrawing();
+
             ClearBackground(BLACK);
+            game.draw();
+        
         EndDrawing();
     }
 
