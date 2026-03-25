c8ea44e5d2132e8d9df9981521f3a045430acf80
DefNotEbbi <ebrahimmuneervayani@gmail.com>
2026-03-26 03:09:28 +0500

    fix background, add setting to toggle grid and setup storage class for db interaction

--- src/spaceInvaders.cpp
+++ src/spaceInvaders.cpp
@@ -58,6 +58,57 @@ enum InputMode{                 // an ENUM to indicate the chosen playerInputMod
 
 // Classes -----
 
+struct LeadeboardItems {
+    int gameID;
+    int timePlayed; // will change these to proper datetime objects later
+    int timeEnded; // same here
+    int date;     // oo cool comment ladder
+    string name;
+    int score;  // consider making long long as im a gigachad gamer
+    int enemiesDefeated;
+};
+
+class Storage {
+    protected:
+        SQLite::Database db;
+
+    public:
+        Storage()
+        : db("Assets/gameData.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE)
+        {
+            db.exec(
+                "CREATE TABLE IF NOT EXISTS leaderboards ("
+                    "gameID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "timePlayed INTEGER,"
+                    "timeEnded INTEGER,"
+                    "date TEXT,"
+                    "name TEXT," 
+                    "score INTEGER,"
+                    "enemiesDefeated INTEGER"
+                ")"
+            );
+
+            db.exec( // add in all the settings and other single variables that need persistence
+                "CREATE TABLE IF NOT EXISTS client ("
+                    "fullscreen BOOLEAN,"
+                    "opacity FLOAT,"
+                    "fps INTEGER" 
+                    "coins INTEGER,"
+                    "wins INTEGER"
+                ")"
+            );
+        }
+
+        void addLeaderboardEntry(const LeadeboardItems& llItems) {
+            
+        }
+
+        void setClient() {} // later
+
+        auto getClient() {} // also for later idk i dont feel it rn
+
+};
+
 class State{                                                        // an abstract class to be inherited by all gameState subclasses
     protected:
         GameState&  gameState;
@@ -69,10 +120,11 @@ class State{                                                        // an abstra
         virtual void update() = 0;
 };
 class Settings : public State{
-    // copied from my Mr.Pong game
+    // copied from your Mr.Pong game
 
     private:
         bool         fullScreen      {false};         // setwindowstate(FLAG_BORDERLESS_WINDOWED_MODE); ClearWindowState(); SetWindowSize();
+        bool         showGrid        {true};          // whether to show the background grid or not
         int          frameRate       {60};            // again, why 63.
         float        windowOpacity   {0.9f};
         float        masterVolume    {0.5};
@@ -83,7 +135,7 @@ class Settings : public State{
         const int    textSize        {35};
         const int    offSet          {100};             // gap between text/heading and option/toggler
         const Color  color           {GOLD};
-        const string texts[5]        {"   > FullScreen     ", "   > FrameRate     ", "   > Window Opacity", "   > SFX Volume    ", "   > Input Mode    "};
+        const string texts[6]        {"   > FullScreen     ", "   > Show Grid      ", "   > FrameRate     ", "   > Window Opacity", "   > SFX Volume    ", "   > Input Mode    "};
 
         Sound settingModifySFX;
 
@@ -111,26 +163,31 @@ class Settings : public State{
             DrawText(texts[0].c_str(), posX, posY, textSize, color);
             DrawText(TextFormat("%s", (fullScreen)? "Enabled" : "Disabled"), posX + MeasureText(texts[0].c_str(), textSize) + 100, posY, textSize, (fullScreen)? GREEN : RED);
 
-            // frame rate
+            // grid toggle
             posY += offSet;
             DrawText(texts[1].c_str(), posX, posY, textSize, color);
-            DrawText(TextFormat("%d", frameRate), posX + MeasureText(texts[1].c_str(), textSize) + 100, posY, textSize, (frameRate == 23)? RED : (frameRate == 40)? ORANGE : (frameRate == 63)? YELLOW : GREEN);
+            DrawText(TextFormat("%s", (showGrid)? "Enabled" : "Disabled"), posX + MeasureText(texts[1].c_str(), textSize) + 100, posY, textSize, (showGrid)? GREEN : RED);
+
+            // frame rate
+            posY += offSet;
+            DrawText(texts[2].c_str(), posX, posY, textSize, color);
+            DrawText(TextFormat("%d", frameRate), posX + MeasureText(texts[2].c_str(), textSize) + 100, posY, textSize, (frameRate == 23)? RED : (frameRate == 40)? ORANGE : (frameRate == 63)? YELLOW : GREEN);
             
             // window opacity
             posY += offSet;
-            DrawText(texts[2].c_str(), posX, posY, textSize, color);
-            DrawText(TextFormat("%.1f", windowOpacity), posX + MeasureText(texts[2].c_str(), textSize) + offSet, posY, textSize, (windowOpacity <= 0.4)? RED : (windowOpacity <= 0.7)? YELLOW : GREEN);
+            DrawText(texts[3].c_str(), posX, posY, textSize, color);
+            DrawText(TextFormat("%.1f", windowOpacity), posX + MeasureText(texts[3].c_str(), textSize) + offSet, posY, textSize, (windowOpacity <= 0.4)? RED : (windowOpacity <= 0.7)? YELLOW : GREEN);
             
             // master volume
             posY += offSet;
-            DrawText(texts[3].c_str(), posX, posY, textSize, color);
-            DrawText(TextFormat("%.0f%%", masterVolume * 100), posX + MeasureText(texts[3].c_str(), textSize) + offSet, posY, textSize, (masterVolume <= 0.4)? RED : (masterVolume <= 0.7)? YELLOW : GREEN);
+            DrawText(texts[4].c_str(), posX, posY, textSize, color);
+            DrawText(TextFormat("%.0f%%", masterVolume * 100), posX + MeasureText(texts[4].c_str(), textSize) + offSet, posY, textSize, (masterVolume <= 0.4)? RED : (masterVolume <= 0.7)? YELLOW : GREEN);
 
             // input mode
             posY += offSet;
-            DrawText(texts[4].c_str(), posX, posY, textSize, color);
+            DrawText(texts[5].c_str(), posX, posY, textSize, color);
             // (playerInputMode == WASD)? BLUE : (playerInputMode == ArrowKeys)? PURPLE : BEIGE);      
-            DrawText(TextFormat("%s", (playerInputMode == WASD)? "WASD Keys" : (playerInputMode == ARROW)? "Arrow Keys" : "Mouse Wheel"), posX + MeasureText(texts[4].c_str(), textSize) + offSet, posY, textSize, BEIGE);  
+            DrawText(TextFormat("%s", (playerInputMode == WASD)? "WASD Keys" : (playerInputMode == ARROW)? "Arrow Keys" : "Mouse Wheel"), posX + MeasureText(texts[5].c_str(), textSize) + offSet, posY, textSize, BEIGE);  
         }
 
         void update(){
@@ -157,9 +214,17 @@ class Settings : public State{
             //     PlaySound(settingModifySFX);
             // }
 
+            // grid toggle
+            posY += offSet;
+            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[1].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("Disabled", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))
+            {
+                showGrid = !showGrid;
+                PlaySound(settingModifySFX);
+            }
+
             // framerate
             posY += offSet;
-            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[1].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("123", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // 123 is the max width of poss values
+            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[2].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("123", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // 123 is the max width of poss values
             {
 
                 switch (frameRate) {
@@ -180,7 +245,7 @@ class Settings : public State{
 
             // window opacity
             posY += offSet;
-            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[2].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("0.1", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // 0.1 is the max width of poss values
+            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[3].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("0.1", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // 0.1 is the max width of poss values
             {
                 windowOpacity += 0.1f;
                 if (windowOpacity >= 1.1f){             // >= needed since float addition can result in smthing like 1.00001f (wahi fpn kay precision ka masla)
@@ -193,7 +258,7 @@ class Settings : public State{
 
             // master volume
             posY += offSet;
-            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[3].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("100%", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // 100% is the max width of poss values
+            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[4].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("100%", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // 100% is the max width of poss values
             {
                 masterVolume += 0.1f;
                 if (masterVolume >= 1.1f){             // >= needed since float addition can result in smthing like 1.00001f (wahi fpn kay precision ka masla)
@@ -206,7 +271,7 @@ class Settings : public State{
 
             // input mode
             posY += offSet;
-            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[4].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("Mouse Wheel", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // "Mouse Wheel" is the max width of poss values
+            if (CheckCollisionPointRec(GetMousePosition(), Rectangle{(float) posX + MeasureText(texts[5].c_str(), textSize) + offSet, (float) posY, (float) MeasureText("Mouse Wheel", textSize), (float) textSize}) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || GetMouseWheelMove()))          // "Mouse Wheel" is the max width of poss values
             {
                 playerInputMode = ((playerInputMode == WASD)? ARROW : (playerInputMode == ARROW)? MOUSE : WASD);
                 
@@ -218,6 +283,9 @@ class Settings : public State{
         }
         bool isFullScreen(){
             return fullScreen;
+        }
+        bool isGridEnabled(){
+            return showGrid;
         }
 };
 
@@ -848,7 +916,7 @@ class Grid{
         , backgroundColor(Color{5, 5, 20, 255})                        // dark bluish/black bg 
         {}
 
-        void draw(const bool fullScreen){
+        void draw(const bool fullScreen, const bool showGrid){
             ClearBackground(backgroundColor);
 
             if (fullScreen && !fullScreened){
@@ -858,19 +926,24 @@ class Grid{
                 fullScreened = true;
             }
     
-            // vertical lines
-            for (auto x {0}; x < ((fullScreen)? fullScreenWidth : screenWidth); x += cellSize)
-                DrawLine(x, 0, x, ((fullScreen)? fullScreenHeight : screenHeight), gridColor);
 
-            // horizontal lines  
-            for (auto y {0}; y < ((fullScreen)? fullScreenHeight : screenHeight); y += cellSize)
-                DrawLine(0, y, ((fullScreen)? fullScreenWidth : screenWidth), y, gridColor);
+            if (showGrid){
+                // vertical lines
+                for (auto x {0}; x < ((fullScreen)? fullScreenWidth : screenWidth); x += cellSize)
+                    DrawLine(x, 0, x, ((fullScreen)? fullScreenHeight : screenHeight), gridColor);
+
+                // horizontal lines  
+                for (auto y {0}; y < ((fullScreen)? fullScreenHeight : screenHeight); y += cellSize)
+                    DrawLine(0, y, ((fullScreen)? fullScreenWidth : screenWidth), y, gridColor);
+            }
+
         }
 };
 struct Star{
     float centreX;              // x pos of its circular centre
     float centreY;              // y pos of its centre
     float radius;               // its radius
+    float speed;                // the speed at which the star moves downwards
     float twinkleSpeed;         // the rate at which the star will blink
     float twinkleOffset;        // a random value for offsetting its twinkle/blinking
     float alpha;                // its transparency; for twinkling
@@ -895,6 +968,7 @@ class Stars{
                         (float) GetRandomValue(0, screenWidth),         // centreX
                         (float) GetRandomValue(0, screenHeight),        // centreY
                         (float) GetRandomValue(1, 5) * 0.5f,            // radius; 0.5f since func cant have float arguments
+                        (float) GetRandomValue(1, 100) / 20.0f,         // speed; between 0.05 and 5.0
                         (float) GetRandomValue(1, 10),                  // twinkleSPeed
                         (float) GetRandomValue(0, (2*PI)*100) / 100,    // twinkleOffset; just in case; 0 to 628(2pi * 100 ie) which is the max period of a sin wave
                         (float) 0.4f                                    // alpha
@@ -921,6 +995,13 @@ class Stars{
             for (auto& star : stars){
                 // star.alpha = (baseAlpha + (maxAlpha * (1.0f + sin(currTime * star.twinkleSpeed + star.twinkleOffset))));                         // credits to claude
                 star.alpha = (baseAlpha + ((maxAlpha - baseAlpha) * (1.0f + sin(currTime * star.twinkleSpeed + star.twinkleOffset))/2));            // sin orig returns -1 to 1; offsetting by +1f to make its range 0 to 2f; sin/2 -> 0 to 1f
+
+                // Move stars downwards
+                star.centreY += star.speed;
+                if (star.centreY > GetScreenHeight()){
+                    star.centreY = -star.radius;
+                    star.centreX = (float) GetRandomValue(0, GetScreenWidth());
+                }
             }
         }
         void draw(){
@@ -936,8 +1017,8 @@ class Background {
         Stars stars;                    // a collection of stars which twinkle randomly
 
     public:
-        void draw(const bool fullScreen){
-            grid.draw(fullScreen);
+        void draw(const bool fullScreen, const bool showGrid){
+            grid.draw(fullScreen, showGrid);
             stars.draw();
         }
         void update(){
@@ -949,7 +1030,7 @@ class Game{
     private:
         GameState        gameState;
         Background       backGround;
-        SQLite::Database db;
+        Storage          storageItems;
 
         Settings     settings;
         Menu         menu;
@@ -966,8 +1047,7 @@ class Game{
 
     public:
         Game()                          // overRiding default constructor 
-        : db("Assets/gameData.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE)
-        , gameState(MENU)               // initializing gameState with MENU
+        : gameState(MENU)               // initializing gameState with MENU
         , menu(gameState, settings)
         , play(gameState)
         , shop(gameState) 
@@ -992,17 +1072,10 @@ class Game{
                 UnloadImage(favicon);
             }
             
-            db.exec(
-                "CREATE TABLE IF NOT EXISTS users ("
-                    "id INTEGER PRIMARY KEY,"
-                    "name TEXT," 
-                    "age INTEGER"
-                ")"
-            );
         }
 
         void draw(){                                                    // draws based upon the current gameState
-            backGround.draw(settings.isFullScreen());
+            backGround.draw(settings.isFullScreen(), settings.isGridEnabled());
 
             switch(gameState)
             {
