a853735e0493c613dbc2f49187aec1758f043bd0
Muhammad Saad <saadfenixel@gmail.com>
2026-04-15 00:29:51 +0500

    Update motherShip
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/2_entities/motherShip/motherShip.hpp
+++ src/code/2_entities/motherShip/motherShip.hpp
@@ -1,1 +1,38 @@
+#pragma once
 
+
+#include <vector>
+#include <iostream>
+#include <raylib.h>
+#include "../lasers/lasers.hpp"
+using std::vector;
+
+
+class MotherShip{
+    private:
+        Texture motherShip;
+
+        Vector2 position;           // using instead of posX, posY
+        int     speed;              // speed by which to update position.x
+        float   scale;
+
+        double  lastSpawned;
+        double  spawnDuration;
+        bool    currentlySpawned;
+        double  randomSpawnPause;   // duration after which to spawn the mothership
+        bool    spawnFromLeft;      // will be randomly decided;
+
+        int     hits;
+        int     maxPossibleHits;
+        int     scoreBoost;
+
+
+    public:
+        MotherShip();
+        ~MotherShip();
+
+        void draw();
+        void update(vector<Laser>& lasers, int& score);
+
+        Rectangle motherShipRect();
+};
