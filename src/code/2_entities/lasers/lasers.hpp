052fa676a4487c728b8d7fe3bd4be230bac2a3d6
Muhammad Saad <saadfenixel@gmail.com>
2026-04-15 00:19:43 +0500

    Update lasers
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/2_entities/lasers/lasers.hpp
+++ src/code/2_entities/lasers/lasers.hpp
@@ -1,1 +1,31 @@
+#pragma once
 
+
+#include <raylib.h>
+#include "../../1_core/enums/player.hpp"
+
+
+class Laser{
+    private:
+        float posX;
+        float posY;
+        float width;
+        float height;
+
+        int   speed;
+        bool  active;
+
+        // helper functions
+        void updatePlayerLaser();
+        void updateAlienLaser();
+
+    public:
+        Laser(const int posX, const int posY);
+
+        void update(Player playerType);
+        void draw();
+
+        Rectangle getRect();
+        bool      isActive();
+        void      deActivate();
+};
