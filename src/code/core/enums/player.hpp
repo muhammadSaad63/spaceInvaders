59ec1dd86f4db81089296fa91b7c2c191ca344fe
Muhammad Saad <saadfenixel@gmail.com>
2026-04-14 23:01:19 +0500

    Update core/
    Co-authored-by: alt <k250604@nu.edu.pk>

---
+++ src/code/core/enums/player.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+
+enum Player{
+    USER,                       // used to refer to the user/player/human
+    ALIEN                       // used to refer to the enemy/computer/pc/alien(s)
+};
