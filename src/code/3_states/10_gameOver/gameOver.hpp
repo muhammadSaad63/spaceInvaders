e2730ba0cee9a174af3e1109f933d8eb16d92844
Muhammad Saad <saadfenixel@gmail.com>
2026-04-15 16:18:29 +0500

    Update gameOver
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/10_gameOver/gameOver.hpp
+++ src/code/3_states/10_gameOver/gameOver.hpp
@@ -4,3 +4,21 @@
 #include "../1_state/state.hpp"
 
 
+class GameOver : public State{
+    private:
+        // DataBase& dataBase;
+        // GameData& gameData;
+
+    public:
+        GameOver(GameState& gameState) 
+        : State(gameState)
+        // , dataBase(dataBase)
+        {}
+
+        void draw(){
+
+        }
+        void update(){
+            //
+        }
+};
