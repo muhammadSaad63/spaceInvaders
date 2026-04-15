134434dc402dcf4c1f28619bee36d56c62d9c5b5
Muhammad Saad <saadfenixel@gmail.com>
2026-04-15 16:15:35 +0500

    Update leaderBoards
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/5_leaderBoards/leaderBoards.hpp
+++ src/code/3_states/5_leaderBoards/leaderBoards.hpp
@@ -4,3 +4,21 @@
 #include "../1_state/state.hpp"
 
 
+class LeaderBoards : public State{
+    private:
+        // DataBase& dataBase;
+        // GameData& gameData;
+
+    public:
+        LeaderBoards(GameState& gameState) 
+        : State(gameState)
+        // , dataBase(dataBase)
+        {}
+
+        void draw(){
+
+        }
+        void update(){
+
+        }
+};
