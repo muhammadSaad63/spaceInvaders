3e46d54e8d60d4d36cde42751be4b28879ea5706
Muhammad Saad <saadfenixel@gmail.com>
2026-04-17 16:33:14 +0500

    Purify gameOver
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/10_gameOver/gameOver.cpp
+++ src/code/3_states/10_gameOver/gameOver.cpp
@@ -1,21 +1,3 @@
 #include "gameOver.hpp"
 
 
-class GameOver : public State{
-    private:
-        // DataBase& dataBase;
-        // GameData& gameData;
-
-    public:
-        GameOver(GameState& gameState) 
-        : State(gameState)
-        // , dataBase(dataBase)
-        {}
-
-        void draw(){
-
-        }
-        void update(){
-            //
-        }
-};
