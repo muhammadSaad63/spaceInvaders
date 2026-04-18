0bcce191cc192fbc8c43e2b552bd67dd7b59c8b3
Muhammad Saad <saadfenixel@gmail.com>
2026-04-18 16:29:14 +0500

    Implement Aliens [x - cleaning .hpp]
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/2_entities/aliens/aliens.hpp
+++ src/code/2_entities/aliens/aliens.hpp
@@ -31,31 +31,37 @@ class Alien{
 
 class Aliens{
     private:
+        int                                   waveNum        { 0 };                                                 // the current wave num
+    
+        // swarm (the collection of aliens) info
         const static int                      numRows        { 3 };                                                 // the total num of rows of aliens in the swarm; dunno y but it wasnt working witout static;
         const static int                      numCols        { 5 };                                                 // the num of aliens in each row of the swarm
         array<array<Alien, numCols>, numRows> aliens;                                                               // a 2d static array of Alien(s)
 
-        const static float                    textureScale   { 0.1f };                                              // a float by which to scale each alien texture while drawing
-        const static int                      edgePadding    { 63   };                                              // the horizontal padding on each side of the screen
+        // swarm info
+        Vector2                               swarmPosition  { 0, 0 };                                              // a tuple containing the (x, y) coor of the top-left point of the swarm
+        int                                   swarmDirection { 1    };                                              // unit vector represeting direction of speed: 1 right, -1 left
+        const int                             swarmWidth     { (alienSpacing * (numCols - 1)) + (alienSpacing) };   // the total width/length of the swarm
+        
+        // swarm speed
+        const float                           baseSpeed      { 0.5f      };                                         // ie the min poss speed of each alien
+        float                                 currSpeed      { baseSpeed };                                         // speed of each alien in the swarm in the current wave
+        const float                           acceleration   { 0.15f     };                                         // ie the speed increase of each alien per wave
+        
+        // drawing constants
         const static int                      alienSpacing   { 81   };                                              // horizontal gap between each alien in a row
         const static int                      rowSpacing     { 70   };                                              // vertical gap between each row in the swarm
+        const static int                      edgePadding    { 63   };                                              // the horizontal padding on each side of the screen
+        const static float                    textureScale   { 0.1f };                                              // a float by which to scale each alien texture while drawing
+        
+        // lasers info
+        vector<Laser>                         alienLasers    {      };                                              // a vector to store the index of active aliens in a row
+        float                                 shootTimer     { 0.0f };                                              // the time passed (in s) after the last laser was shot
+        float                                 shootInterval  { 2.0f };                                              // the time interval/duration bw each consecutive laser firing
 
-        Vector2                               swarmPosition  { 0, 0 };                                              // 
-        int                                   swarmDirection { 1 };                                                 // unit vector represeting direction of speed: 1 right, -1 left
-        const int                             swarmWidth     { (alienSpacing * (numCols - 1)) + (alienSpacing) };   // 
-
-        int         waveNum      { 0 };                                                                             // the current wave num
-        const float baseSpeed    { 0.5f };                                                                          // ie the min poss speed of each alien
-        float       currSpeed    { baseSpeed };                                                                     // speed of each alien in the swarm in the current wave
-        const float acceleration { 0.15f };                                                                         // ie the speed increase of each alien per wave
-
-        vector<Laser> alienLasers;
-        float shootTimer    { 0.0f };
-        float shootInterval { 2.0f };
-
-        // internal helpers
-        bool hittingLeftEdge();
-        bool hittingRightEdge();
+        // internal, helper methods
+        bool  hittingLeftEdge();
+        bool  hittingRightEdge();
 
         void  centerSwarm();
         float calcSwarmSpeed();
@@ -63,8 +69,8 @@ class Aliens{
         bool  isSwarmDestroyed();
         void  loadNextWave();
 
-        bool getRandomActiveAlien(int& outRow, int& outCol);
-        void shoot();
+        bool  getRandomActiveAlien(int& outRow, int& outCol);
+        void  shootALaser();
 
     public:
         Aliens();
@@ -81,6 +87,7 @@ class Aliens{
         
         int  getWaveNum();
         bool allDestroyed();
+
         // returns the world-space rect for alien at [row][col]
         Rectangle getAlienRect(int row, int col);
 };
