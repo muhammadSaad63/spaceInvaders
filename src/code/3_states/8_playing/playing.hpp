723c69030528ec7c904f1e30f6cefc77d6b0ec00
Muhammad Saad <saadfenixel@gmail.com>
2026-04-19 11:41:40 +0500

    Revamp Playing
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/8_playing/playing.hpp
+++ src/code/3_states/8_playing/playing.hpp
@@ -14,17 +14,17 @@
 
 class Playing : public State{
     private:
-        Aliens         aliens;                       // composition of aliens (swarm of alien)
-        SpaceShip      spaceShip;                    // composition of spaceship
-        MotherShip     motherShip;                   // composition of mothership
+        Aliens         aliens;                              // composition of aliens (swarm of alien)
+        SpaceShip      spaceShip;                           // composition of spaceship
+        MotherShip     motherShip;                          // composition of mothership
 
-        InputMode&     movementMode;                 // for storing reference of playerInputMode from settings
-        vector<Laser>& aliensLasers;                 // reference of laser vector from aliens
-        vector<Laser>& spaceShipLasers;              // for storing reference of lasers from spaceShip
+        InputMode&     movementMode;                        // for storing reference of playerInputMode from settings
+        vector<Laser>& aliensLasers;                        // reference of laser vector from aliens
+        vector<Laser>& spaceShipLasers;                     // for storing reference of lasers from spaceShip
         
-        int            gameScore       {0};          // score reached in the game session
-        int            enemiesDefeated {0};          // total number of enemies defeated
-        int            livesRemaining  {3};          // the number of player/spaceShip lives remaining
+        int            gameScore            {0};            // score reached in the game session
+        int            enemiesDefeated      {0};            // total number of enemies defeated
+        int            playerLivesRemaining {3};            // the number of player/spaceShip lives remaining
 
     public:
         Playing(GameState& gameState, Settings& settings);
