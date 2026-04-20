a5471bbf197a0fe11df298e0ee49086bdb491a70
Muhammad Saad <saadfenixel@gmail.com>
2026-04-20 23:08:16 +0500

    {#35} Implement statistics.cpp
    Co-authored-by: alt <k250604@nu.edu.pk>
    Co-authored-by: Ebbi <ebrahimmuneervayani@gmail.com>
    Co-authored-by: Umair <m.umairahmed21@gmail.com>

--- src/code/3_states/5_statistics/statistics.cpp
+++ src/code/3_states/5_statistics/statistics.cpp
@@ -1,11 +1,31 @@
+#pragma once
+
+
+#include <raylib.h>
 #include "statistics.hpp"
 
 
-Statistics::Statistics(GameState& gameState) : State(gameState) {}
+// internal, helper method(s)
+void Statistics::loadStats(){
+    if (!statsLoaded){
+        stats       = dataBase.getStatistics();
+        statsLoaded = true;
+    }
+}
+
+Statistics::Statistics(GameState& gameState, DataBase& dataBase)
+            : State(gameState), dataBase(dataBase)
+            {}
 
 void Statistics::draw(){
-
+    //
 }
 void Statistics::update(){
+    loadStats();
 
+    // exiting from history state
+    if (IsKeyPressed(KEY_ENTER)){
+        statsLoaded = false;               // so that stats will be reloaded fresh when state is again entered
+        gameState   = MENU;                // returns to menu effectively
+    }
 }
