60d2b30276ad59be0c1065e5fbfe66c854e19fd0
Muhammad Saad <saadfenixel@gmail.com>
2026-04-22 01:04:09 +0500

    {#38} Implement gameOver
    Co-authored-by: alt <k250604@nu.edu.pk>
    Co-authored-by: Ebbi <ebrahimmuneervayani@gmail.com>
    Co-authored-by: Umair <m.umairahmed21@gmail.com>

--- src/code/3_states/10_gameOver/gameOver.hpp
+++ src/code/3_states/10_gameOver/gameOver.hpp
@@ -2,25 +2,20 @@
 
 
 #include "../1_state/state.hpp"
-#include "../../1_core/dataBase/dataBase.hpp"
+#include "../8_playing/playing.hpp"
 #include "../../1_core/enums/gameState.hpp"
+#include "../../1_core/dataBase/dataBase.hpp"
 
 
 class GameOver : public State{
     private:
         DataBase& dataBase;
-        // GameData& gameData;
+        Playing&  playing;
+        float     timer;
 
     public:
-        GameOver(GameState& gameState, DataBase& dataBase) 
-        : State(gameState)
-        , dataBase(dataBase)
-        {}
+        GameOver(GameState& gameState, DataBase& dataBase, Playing& playing);
 
-        void draw(){
-
-        }
-        void update(){
-            //
-        }
+        void draw();
+        void update();
 };
