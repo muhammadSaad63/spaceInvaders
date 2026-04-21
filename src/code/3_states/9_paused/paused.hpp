c1f60c9658342c851f5346fcf35b943f184555be
Muhammad Saad <saadfenixel@gmail.com>
2026-04-22 01:04:54 +0500

    {#38} Update paused
    Co-authored-by: alt <k250604@nu.edu.pk>
    Co-authored-by: Ebbi <ebrahimmuneervayani@gmail.com>
    Co-authored-by: Umair <m.umairahmed21@gmail.com>

--- src/code/3_states/9_paused/paused.hpp
+++ src/code/3_states/9_paused/paused.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
 
-#include <raylib.h>
 #include "../1_state/state.hpp"
 #include "../8_playing/playing.hpp"
-#include "../../1_core/enums/gameState.hpp"
 
 
 class Paused : public State{
     private:
         Playing& playing;
-        int      textSize  {83};
         Sound    gameResumedSFX;
         Sound    gameStoppedSFX;
+        int      textSize { 50 };
 
     public:
         Paused(GameState& gameState, Playing& playing);
