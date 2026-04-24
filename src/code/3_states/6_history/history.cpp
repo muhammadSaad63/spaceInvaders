ea9c6d08ed8573f64a8036206c27d2304d9e4751
Muhammad Saad <saadfenixel@gmail.com>
2026-04-24 12:55:13 +0500

    Update History.hpp
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/6_history/history.cpp
+++ src/code/3_states/6_history/history.cpp
@@ -3,26 +3,63 @@
 
 
 // helpers
+void History::drawHeader(int& posY){
+    DrawText("History", posX, posY, textSize + 10, GOLD);
+    posY += (textSize + 10) + 15;
+
+    DrawLineEx({(float)posX, (float)posY}, {(float)GetScreenWidth() - posX, (float)posY}, 1, DARKGRAY);
+    posY += 30;
+}
+void History::drawHistory(int& posY){
+    //
+}
+void History::drawOverLay(){
+        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), ColorAlpha(BLACK, 0.5f));
+        DrawText("No games played yet  :(",
+                    GetScreenWidth()/2  - MeasureText("No games played yet  :(", 40)/2,
+                    GetScreenHeight()/2 - 20, 40, DARKGRAY);
+}
+
 void History::loadHistory(){
-    if (!entriesLoaded){
-        entriesLoaded = true;
-        entries       = dataBase.getHistory(maxEntriesToFetch);
+    if (!historyLoaded){
+        historyLoaded = true;
+        history       = dataBase.getHistory(maxEntriesToFetch);
     }
 }
 
 History::History(GameState& gameState, DataBase& dataBase)
-: State(gameState), dataBase(dataBase)
-{}
+        : State(gameState), dataBase(dataBase)
+        {
+            stateChangedSFX = LoadSound("src/assets/sounds/sfx/stateChanged.mp3");
+        }
+History::~History(){
+    UnloadSound(stateChangedSFX);
+}
 
 void History::draw(){
-    //
+    int posY = initPosY;  
+
+    // header
+    drawHeader(posY);
+
+    // table
+    drawHistory(posY);
+
+    // exit hint matches settings style
+    DrawText("Press ENTER to go back", GetScreenWidth() - MeasureText("Press ENTER to go back", textSize) - 23, GetScreenHeight() - 40, textSize, GOLD);
+
+    // making a dark black rectangular overlay over screen if no stats there
+    if (!historyLoaded || !history.empty())
+        drawOverLay();
 }
 void History::update(){
     loadHistory();
 
     // exiting from history state
     if (IsKeyPressed(KEY_ENTER)){
-        entriesLoaded    = false;               // so that entries will be reloaded when state again entered
+        historyLoaded    = false;               // so that history will be reloaded when state again entered
+        PlaySound(stateChangedSFX);
+
         gameState        = MENU;                // return  to menu effectively
     }
 }
--- src/code/3_states/6_history/history.hpp
+++ src/code/3_states/6_history/history.hpp
@@ -10,14 +10,28 @@ class History : public State{
     private:
         DataBase&        dataBase;
 
-        vector<GameData> entries;
-        bool             entriesLoaded;
+        vector<GameData> history;
+        bool             historyLoaded;
         int              maxEntriesToFetch;
+
+        // drawing constants
+        const int posX     { 23  };
+        const int initPosY { 23  };
+        const int textSize { 25  };
+        const int offset   { 450 };   // gap between label and value 
+
+        Sound     stateChangedSFX;
+
+
+        void drawHeader(int& posY);
+        void drawHistory(int& posY);
+        void drawOverLay();
 
         void loadHistory();
 
     public:
         History(GameState& gameState, DataBase& dataBase);
+        ~History();
 
         void draw();
         void update();
