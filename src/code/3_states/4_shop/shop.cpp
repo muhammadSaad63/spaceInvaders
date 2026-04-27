f759afa7f3cdfbd284b2ff8196fc805705547948
Muhammad Saad <saadfenixel@gmail.com>
2026-04-27 06:09:08 +0500

    Update Shop.cpp
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/4_shop/shop.cpp
+++ src/code/3_states/4_shop/shop.cpp
@@ -1,16 +1,32 @@
 #include "shop.hpp"
-#include <raylib.h>
 
 
 Shop::Shop(GameState& gameState)
     : State(gameState) 
-    {}
+    , memePlayed(false)
+    {
+        shopWowSFX = LoadSound("src/assets/sounds/sfx/shopWow.mp3");
+        wowSoEmpty = LoadTexture("src/assets/graphics/ui/misc/wowSoEmpty.jpg");
+    }
+Shop::~Shop(){
+    UnloadSound(shopWowSFX);
+    UnloadTexture(wowSoEmpty);
+}
 
 void Shop::draw(){
-    DrawText("Press ENTER to go back", GetScreenWidth() - MeasureText("Press ENTER to go back", 23) - 23, GetScreenHeight() - 40, 23, GOLD);
+    DrawTexture(wowSoEmpty, (GetScreenWidth()/2 - wowSoEmpty.width/2), (GetScreenHeight()/2 - wowSoEmpty.height/2), WHITE);
+    DrawText("Press ENTER to go back", (GetScreenWidth() - MeasureText("Press ENTER to go back", 23) - 23), (GetScreenHeight() - 40), 23, GOLD);
 }
 void Shop::update(){
+    if (!memePlayed){
+        PlaySound(shopWowSFX);
+        memePlayed = true;
+
+        return;
+    }
+
     if (IsKeyPressed(KEY_ENTER)){
-        gameState        = MENU;                // return  to menu effectively
+        memePlayed = false;
+        gameState  = MENU;                         // return  to menu effectively
     }
 }
