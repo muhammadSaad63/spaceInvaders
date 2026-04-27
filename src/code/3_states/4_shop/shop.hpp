168539c1ef3d4f687027335f45406760446ea0f1
Muhammad Saad <saadfenixel@gmail.com>
2026-04-27 06:00:14 +0500

    Update Shop.hpp
    Co-authored-by: alt <k250604@nu.edu.pk>

--- src/code/3_states/4_shop/shop.hpp
+++ src/code/3_states/4_shop/shop.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 
+#include <raylib.h>
 #include "../1_state/state.hpp"
 
 
 class Shop : public State{
     private:
-        //
+        Texture wowSoEmpty;
+        bool    memePlayed;
 
     public:
         Shop(GameState& gameState);
+        ~Shop();
 
         void draw();
         void update();
